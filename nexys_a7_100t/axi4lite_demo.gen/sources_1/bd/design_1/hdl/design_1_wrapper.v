0e51bc7056c4cb897744c80d47c1dd2ee1bc402f
DouglasWWolf <douglaswolf@yahoo.com>
2022-06-11 14:20:16 -0700

    No commit message

---
+++ nexys_a7_100t/axi4lite_demo.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -0,0 +1,32 @@
+//Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2021.1 (lin64) Build 3247384 Thu Jun 10 19:36:07 MDT 2021
+//Date        : Sat Jun 11 14:04:08 2022
+//Host        : simtool5-2 running 64-bit Ubuntu 20.04.4 LTS
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   (CLK100MHZ,
+    CPU_RESETN,
+    GPIO_BTNU,
+    GPIO_LED);
+  input CLK100MHZ;
+  input CPU_RESETN;
+  input GPIO_BTNU;
+  output [3:0]GPIO_LED;
+
+  wire CLK100MHZ;
+  wire CPU_RESETN;
+  wire GPIO_BTNU;
+  wire [3:0]GPIO_LED;
+
+  design_1 design_1_i
+       (.CLK100MHZ(CLK100MHZ),
+        .CPU_RESETN(CPU_RESETN),
+        .GPIO_BTNU(GPIO_BTNU),
+        .GPIO_LED(GPIO_LED));
+endmodule
